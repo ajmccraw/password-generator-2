bcd2a601b87440bb0fd236fc90fa9fc291931dd0
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-12-29 16:51:17 -0600

    added document variables in javascript file

--- index.js
+++ index.js
@@ -16,3 +16,16 @@ let withoutNumbers = false;
 
 let isNumbersSelected = true;
 let isSymbolsSelected = true;
+
+let generatorBtn = document.getElementById("generator-btn");
+let symbolsBtn = document.querySelector(".symbols-on");
+let numbersBtn = document.querySelector(".numbers-on");
+let passwordOne = document.getElementById("password-one");
+let passwordTwo = document.getElementById("password-two");
+let darkLightmodeImg = document.getElementById("darklightmode-img");
+let container = document.querySelector(".container");
+let containerH1 = document.querySelector(".title-dark");
+let containerDesc = document.querySelector(".desc-dark");
+let hr = document.querySelector(".hr-dark");
+let copyOne = document.getElementById("copy-text-one");
+let copyTwo = document.getElementById("copy-text-two");
