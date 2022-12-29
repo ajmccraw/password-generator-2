297b2f178909ed7f5b1cc2bc08d279ede6da7f86
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-12-29 16:44:47 -0600

    added variables in index.js

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ const charactersNoSymbols = ["A","B","C","D","E","F","G","H","I","J","K","L","M"
 const charactersNoSymbolsNoNumbers = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 const charactersNoNumbers = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"];
+
+let bothSymbolsNumbers = true;
+let onlyLetters = false;
+let onlyLettersNumbers = false;
+let onlyLettersSymbols = false;
+let withoutSymbols = false;
+let withoutNumbers = false;
+
+let isNumbersSelected = true;
+let isSymbolsSelected = true;
